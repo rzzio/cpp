b2e6fee9d5c96a397990336e1db0b39070c83a69
umesh <36256371+hancyboy@users.noreply.github.com>
2019-02-26 19:01:48 +0545

    Add files via upload

---
+++ Menu.cpp
@@ -0,0 +1,65 @@
+#include "Menu.h"
+
+
+Menu::Menu(float width, float height)
+{
+	if (!font.loadFromFile("font/arial.ttf"))
+	{
+		// handle error
+	}
+
+	menu[0].setFont(font);
+	menu[0].setFillColor(sf::Color::Red);
+	menu[0].setString("Play");
+	menu[0].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
+
+	menu[1].setFont(font);
+	menu[1].setFillColor(sf::Color::White);
+	menu[1].setString("About");
+	menu[1].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+
+	menu[2].setFont(font);
+	menu[2].setFillColor(sf::Color::White);
+	menu[2].setString("Help");
+	menu[2].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
+
+	menu[3].setFont(font);
+	menu[3].setFillColor(sf::Color::White);
+	menu[3].setString("Exit");
+	menu[3].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 4));
+
+	selectedItemIndex = 0;
+}
+
+
+Menu::~Menu()
+{
+}
+
+void Menu::draw(sf::RenderWindow &window)
+{
+	for (int i = 0; i < MAX_NUMBER_OF_ITEMS; i++)
+	{
+		window.draw(menu[i]);
+	}
+}
+
+void Menu::MoveUp()
+{
+	if (selectedItemIndex - 1 >= 0)
+	{
+		menu[selectedItemIndex].setFillColor(sf::Color::White);
+		selectedItemIndex--;
+		menu[selectedItemIndex].setFillColor(sf::Color::Red);
+	}
+}
+
+void Menu::MoveDown()
+{
+	if (selectedItemIndex + 1 < MAX_NUMBER_OF_ITEMS)
+	{
+		menu[selectedItemIndex].setFillColor(sf::Color::White);
+		selectedItemIndex++;
+		menu[selectedItemIndex].setFillColor(sf::Color::Red);
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,357 +1,65 @@
-#include <SFML/Graphics.hpp>
-
-#include"player1.h"
-
-using namespace sf;
-
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include "Menu.h"
 
 int main()
-
 {
-	srand(time(NULL));
-	sf::RenderWindow window;
-	window.create(VideoMode(1000, 800), "archerman", Style::Titlebar | Style::Close);
-	window.setFramerateLimit(60);
-	sf::Texture texture;
-	if (!texture.loadFromFile("backimg.png"))
-		std::cout << "failed load";
-	sf::Sprite bg;
-	bg.setTexture(texture);
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML WORK!");
 
-
-	Font font;     //making my font 
-	font.loadFromFile("28 Days Later.ttf");
-
-	//init textures
-	Texture playertex;
-	playertex.loadFromFile("archer.png");
-
-
-	Texture enemytex;
-	enemytex.loadFromFile("enemy.png");
-
-	Texture earrow;
-	earrow.loadFromFile("earrow.png");
-
-
-	Texture bullettex;
-	bullettex.loadFromFile("arrow.png");
-
-	player player2(&enemytex); //making 2nd player
-	int score2 = 0;
-	int shoottimer2 = 50;
-
-
-	//displaying scoretext
-	Text scoretext;
-	scoretext.setFont(font);
-	scoretext.setCharacterSize(25);
-	scoretext.setFillColor(Color::Red);
-	scoretext.setPosition(10.f, 10.f);
-
-	//player init
-	int score = 0;                            //making 1st player
-	player player(&playertex);
-	int shoottimer = 40;
-
-	Text hptext;
-	hptext.setFont(font);
-	hptext.setCharacterSize(12);
-	//hptext.setColor(Color::White);
-
-	//init enimies
-	int enemyspawntimer = 5;
-	std::vector<enemy> enemies;
-
-	Text ehptext;
-	ehptext.setFont(font);
-	ehptext.setCharacterSize(20);
-	//ehptext.setColor(Color::White);
-
-	Text gameover;
-	gameover.setFont(font);
-	gameover.setCharacterSize(100);
-	gameover.setFillColor(Color::Red);
-	gameover.setPosition(100.f, player.shape.getPosition().x / 2);
-	gameover.setString("GAME OVER BUDDY");
-
-
-
+	Menu menu(window.getSize().x, window.getSize().y);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
+
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					menu.MoveUp();
+					break;
+
+				case sf::Keyboard::Down:
+					menu.MoveDown();
+					break;
+
+				case sf::Keyboard::Return:
+					switch (menu.GetPressedItem())
+					{
+					case 0:
+						std::cout << "Play button has been pressed" << std::endl;
+						break;
+					case 1:
+						std::cout << "About button has been pressed" << std::endl;
+						break;
+					case 2:
+						std::cout << "Help button has been pressed" << std::endl;
+						break;
+					case 3:
+						window.close();
+						break;
+					}
+
+					break;
+				}
+
+				break;
+			case sf::Event::Closed:
 				window.close();
 
+				break;
+
+			}
 		}
 
-		//update player
-		if (player.hp > 0)
-		{
-
-
-			//making 1st player move
-
-			if (Keyboard::isKeyPressed(Keyboard::W))
-				player.shape.move(0.f, -10.f);
-
-			if (Keyboard::isKeyPressed(Keyboard::A))
-				player.shape.move(-10.f, 0.f);
-
-			if (Keyboard::isKeyPressed(Keyboard::S))
-				player.shape.move(0.f, 10.0f);
-
-			if (Keyboard::isKeyPressed(Keyboard::D))
-				player.shape.move(10.f, 0.f);
-
-			hptext.setPosition(player.shape.getPosition().x, player.shape.getPosition().y - hptext.getGlobalBounds().height);
-			hptext.setString(std::to_string(player.hp) + "/" + std::to_string(player.hpmax));
-
-			//making 2nd player move___________________________________________________________________
-
-			if (Keyboard::isKeyPressed(Keyboard::Up))
-				player2.shape.move(0.f, -10.f);
-
-			if (Keyboard::isKeyPressed(Keyboard::Left))
-				player2.shape.move(-10.f, 0.f);
-
-			if (Keyboard::isKeyPressed(Keyboard::Down))
-				player2.shape.move(0.f, 10.0f);
-
-			if (Keyboard::isKeyPressed(Keyboard::Right))
-				player2.shape.move(10.f, 0.f);
-
-			hptext.setPosition(player2.shape.getPosition().x, player2.shape.getPosition().y - hptext.getGlobalBounds().height);
-			hptext.setString(std::to_string(player2.hp) + "/" + std::to_string(player2.hpmax));
-
-
-
-			//_____________________________________________________________________________________________
-
-
-			//collison with window of player1
-
-			if (player.shape.getPosition().x <= 0) //left 
-				player.shape.setPosition(0.f, player.shape.getPosition().y);
-			if (player.shape.getPosition().x >= window.getSize().x - player.shape.getGlobalBounds().width) //right
-				player.shape.setPosition(window.getSize().x - player.shape.getGlobalBounds().width, player.shape.getPosition().y);
-			if (player.shape.getPosition().y <= 0) //top
-				player.shape.setPosition(player.shape.getPosition().x, 0.0f);
-			if (player.shape.getPosition().y >= window.getSize().y - player.shape.getGlobalBounds().height) //bottom
-				player.shape.setPosition(player.shape.getPosition().x, window.getSize().y - player.shape.getGlobalBounds().height);
-
-
-
-
-			//collison with window of player 2_______________________________
-
-			if (player2.shape.getPosition().x <= 0) //left 
-				player2.shape.setPosition(0.f, player2.shape.getPosition().y);
-			if (player2.shape.getPosition().x >= window.getSize().x - player2.shape.getGlobalBounds().width) //right
-				player2.shape.setPosition(window.getSize().x - player2.shape.getGlobalBounds().width, player2.shape.getPosition().y);
-			if (player2.shape.getPosition().y <= 0) //top
-				player2.shape.setPosition(player2.shape.getPosition().x, 0.0f);
-			if (player2.shape.getPosition().y >= window.getSize().y - player2.shape.getGlobalBounds().height) //bottom
-				player2.shape.setPosition(player2.shape.getPosition().x, window.getSize().y - player2.shape.getGlobalBounds().height);
-
-
-
-			//______________________________________________________________
-
-
-
-
-
-
-
-
-
-			//update comtrol
-
-			// bullets of player 1
-			if (shoottimer < 40)
-			{
-				shoottimer++;
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Space) && shoottimer >= 40)
-
-			{
-				player.bullets.push_back(bullet(&bullettex, player.shape.getPosition()));
-				shoottimer = 0;
-			}
-			// bullets
-
-
-
-			//______________________________________________________ bullets of player 2
-
-
-			if (shoottimer2 < 40)
-			{
-				shoottimer2++;
-			}
-			if (Keyboard::isKeyPressed(Keyboard::RShift) && shoottimer2 >= 40)
-			{
-				player2.bullets.push_back(bullet(&earrow, player2.shape.getPosition()));
-				shoottimer2 = 0;
-			}
-
-			if (shoottimer2 < 15)
-			{
-				shoottimer2++;
-			}
-
-			//____________________________________________________
-
-
-
-			//bullets window collision of player 1
-			for (size_t i = 0; i < player.bullets.size(); i++)
-			{
-				//move bullets
-				player.bullets[i].shape.move(20.0F, 5.F);
-				if (player.bullets[i].shape.getPosition().x > window.getSize().x)
-				{
-					player.bullets.erase(player.bullets.begin() + i);
-					break;
-				}
-				if (player.bullets[i].shape.getGlobalBounds().intersects(player2.shape.getGlobalBounds()))
-				{
-					score++;
-					player.bullets.erase(player.bullets.begin() + i);
-					break;
-
-				}
-
-
-
-
-
-			}
-
-			//bullet window colliosn of player 2________________________
-			for (size_t i = 0; i < player2.bullets.size(); i++)
-			{
-				//move bullets
-				player2.bullets[i].shape.move(-20.0F, -5.F);
-				if (player2.bullets[i].shape.getPosition().x > window.getSize().x)
-				{
-					player2.bullets.erase(player2.bullets.begin() + i);
-					break;
-				}
-
-				if (player2.bullets[i].shape.getGlobalBounds().intersects(player.shape.getGlobalBounds()))
-				{
-					score2++;
-					player2.bullets.erase(player2.bullets.begin() + i);
-					break;
-
-				}
-
-
-
-
-				//enemy collision
-				
-			}
-
-			//________________________________________________
-
-
-
-			//enemy update
-			if (enemyspawntimer < 50)   //ENEMY SPAWN
-				enemyspawntimer++;
-
-
-			if (enemyspawntimer >= 1000)
-			{
-
-				enemies.push_back(enemy(&enemytex, window.getSize()));
-				enemyspawntimer = 0;
-			}
-
-
-
-			for (size_t i = 0; i < enemies.size(); i++)
-			{
-				enemies[i].shape.move(-7.4f, 0.f);
-
-				if (enemies[i].shape.getPosition().x <= 0 - enemies[i].shape.getGlobalBounds().width)
-				{
-					enemies.erase(enemies.begin() + i);
-					break;
-				}
-				if (enemies[i].shape.getGlobalBounds().intersects(player.shape.getGlobalBounds()))
-				{
-					enemies.erase(enemies.begin() + i);
-					player.hp--; //player health decreasing
-					break;
-
-				}
-			}
-
-			//UI update
-			scoretext.setString("Score=" + std::to_string(score));
-
-		}
-
-		//============================================================================//
-
-
-
-		//draw 
 		window.clear();
-		window.draw(bg);
 
-
-
-
-		window.draw(player.shape);
-		window.draw(player2.shape);
-
-
-
-
-		//bullets
-		for (size_t i = 0; i < player.bullets.size(); i++)
-		{
-			window.draw(player.bullets[i].shape);
-
-		}
-
-		//bullets of player 2
-
-		for (size_t i = 0; i < player2.bullets.size(); i++)
-		{
-			window.draw(player2.bullets[i].shape);
-		}
-
-		//enemy
-		for (size_t i = 0; i < enemies.size(); i++)
-		{
-			ehptext.setString(std::to_string(enemies[i].hp) + "/" + std::to_string(enemies[i].hpmax));
-			ehptext.setPosition(enemies[i].shape.getPosition().x, enemies[i].shape.getPosition().y - ehptext.getGlobalBounds().height);
-			//window.draw(ehptext);
-			window.draw(enemies[i].shape);
-		}
-
-		//UI
-		window.draw(scoretext);
-		window.draw(hptext);
-
-
-		if (player.hp <= 0)
-			window.draw(gameover);
+		menu.draw(window);
 
 		window.display();
-
-
-
 	}
-
-	return 0;
 }
